47ba43365a3e5432f3df5406532df2e573e252d9
Arnaud Mouttapa <arnaudmouttapa@gmail.com>
2024-07-27 15:48:11 +0200

    put graph parameters in one line

--- chatbot_api/src/chains/hospital_cypher_chain.py
+++ chatbot_api/src/chains/hospital_cypher_chain.py
@@ -8,11 +8,7 @@ HOSPITAL_QA_MODEL = os.getenv("HOSPITAL_QA_MODEL")
 HOSPITAL_CYPHER_MODEL = os.getenv("HOSPITAL_CYPHER_MODEL")
 
 
-graph = Neo4jGraph(
-    url=os.getenv("NEO4J_URI"),
-    username=os.getenv("NEO4J_USER"),
-    password=os.getenv("NEO4J_PASSWORD"),
-)
+graph = Neo4jGraph(url=os.getenv("NEO4J_URI"),username=os.getenv("NEO4J_USER"),password=os.getenv("NEO4J_PASSWORD"))
 
 graph.refresh_schema()
 
